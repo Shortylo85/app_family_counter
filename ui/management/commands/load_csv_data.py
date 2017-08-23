0cf0ab068ad3c39e52c17f4559ffa577fe9874ce
Shortylo85 <daneshorty@gmail>
2017-08-23 23:30:51 +0200

    ui/models.py and ui/management/commands/load_csv_data.py initiaded and prepared for running

---
+++ ui/management/commands/load_csv_data.py
@@ -0,0 +1,35 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from ui.models import City
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+
+    def handle(self, **args):
+        print("Start")
+        
+        directory = "/home/danijel/Downloads/csv_files/family_counter/GeoLite2-City-CSV_20170801"
+        
+        locations = open(os.path.join(directory, 'GeoLiteCity-Location.csv'))
+        
+        csv_file = csv.reader(locations)
+        
+        counter = 0
+        
+        for row in csv_file:
+            
+            data = {
+               'city_read': row[3],
+                'lat': row[5],
+                'lng': row[6],
+            }
+            
+            City.objects.get_or_create(data)
+            
+            if counter % 1000 == 0:
+                print("imported {} entries".format(counter))
+            counter += 1
--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField(max_length=64,null=True,blank=True)
+    lat = models.DecimalField(max_digits=7,decimal_places=4)
+    lng = models.DecimalField(max_digits=7,decimal_places=4)
+    
+class UserCity(models.Model):
+    user = models.ForeignKey(User)
+    city = models.ForeignKey(City)
