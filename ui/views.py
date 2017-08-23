f69af641311172c846937a2986148ed55457319a
Shortylo85 <daneshorty@gmail>
2017-08-23 18:18:15 +0200

    Custom registration form created

--- ui/views.py
+++ ui/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from ui.forms import RegistrationForm
+
 
 # Create your views here.
 def index(request):
-    return render(request, template_name = "ui/index.html")
+    
+    built_context ={ 'user': request.user }
+    return render(request, template_name = "ui/index.html", context = built_context)
 
 def getMap(request):
     return render(request, template_name = 'ui/map.html')
 
 def register(request):
-    return render(request, template_name = 'account/register.html')
+    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+         
+    args = {'form':form}        
+     
+    return render(request, template_name = 'account/register.html', context = args)
+    
 
 def login(request):
     return render(request, template_name = 'account/login.html')
